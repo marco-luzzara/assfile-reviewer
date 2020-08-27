1375d07df6f15adc7bd68236c31110446b0f2648
Marco Luzzara <marco.luzzara@hotmail.it>
2020-08-27 11:23:09 +0200

    everything between {}, used to format subtitles, is now ignored during the review (only)

--- review.py
+++ review.py
@@ -6,19 +6,30 @@ import os
 
 dialogueRegex = re.compile(r'^(Dialogue:(?:[^,]*?,){9})(.*?)$')
 capitalPunctuationRegex = re.compile(r'([?!.]\s*)(\w)')
+AssFormattingRegex = re.compile(r'\{.*?\}')
 
 class LineReviewer:
     def __init__(self, initialLine: str, maxLineLength: int):
-        self.line = initialLine
-        self.maxLen = maxLineLength
-    
+        self._line = initialLine
+        self._maxLen = maxLineLength
+        self._line = self._line.replace('\\N', '')
+
+        self._formattingMatches = []
+        formattingMatch = AssFormattingRegex.search(self._line)
+        while formattingMatch != None:
+            startPos = formattingMatch.start(0)
+            formattingMatchStr = formattingMatch.group(0)
+            self._formattingMatches.append([startPos, formattingMatchStr])
+            self._line = self._line.replace(formattingMatchStr, '', 1)
+            formattingMatch = AssFormattingRegex.search(self._line, startPos)
+
     def capitalizeLine(self) -> LineReviewer:
-        if len(self.line) > 0:
-            self.line = self.line[0].upper() + self.line[1:]
+        if len(self._line) > 0:
+            self._line = self._line[0].upper() + self._line[1:]
         return self
 
     def upperCaseForCapitalPunctuation(self) -> LineReviewer:
-        self.line = capitalPunctuationRegex.sub(lambda match: match.group(1) + match.group(2).upper(), self.line)
+        self._line = capitalPunctuationRegex.sub(lambda match: match.group(1) + match.group(2).upper(), self._line)
         return self
 
     def __findNearestWhiteSpace(self, line: str, pointer: int) -> int:
@@ -37,12 +48,12 @@ class LineReviewer:
         return pointer if minWithSign[0] == math.inf else pointer + minWithSign[0] * minWithSign[1]
 
     def splitLineWhenGreaterThanMaxLen(self) -> LineReviewer:
-        if self.maxLen == 0:
+        if self._maxLen == 0:
             return self
 
         lineChunks = []
-        residualLine = self.line
-        nLines = math.ceil(len(self.line) / self.maxLen)
+        residualLine = self._line
+        nLines = math.ceil(len(self._line) / self._maxLen)
 
         while nLines > 0:
             nlIndex = None
@@ -55,15 +66,35 @@ class LineReviewer:
             residualLine = residualLine[nlIndex:]
             nLines -= 1
 
-        self.line = '\\N'.join(lineChunks)
+        self._line = '\\N'.join(lineChunks)
         return self
+
+    def getReviewedLine(self) -> str:
+        lastIndex = 0
+        newLineIndex = self._line.find('\\N', lastIndex)
+
+        while newLineIndex != -1:
+            for m in self._formattingMatches:
+                if m[0] >= newLineIndex:
+                    m[0] = m[0] + 2
+
+            lastIndex = newLineIndex + 2
+            newLineIndex = self._line.find('\\N', lastIndex)
+
+        offset = 0
+        for m in self._formattingMatches:
+            curIndex = m[0] + offset
+            self._line = self._line[:curIndex] + m[1] + self._line[curIndex:]
+            offset = offset + len(m[1])
+
+        return self._line
 
 
 def reviewLine(line: str, maxLineLength: int) -> str:
     reviewer = LineReviewer(line, maxLineLength)
     reviewer.capitalizeLine().upperCaseForCapitalPunctuation().splitLineWhenGreaterThanMaxLen()
 
-    return reviewer.line
+    return reviewer.getReviewedLine()
 
 
 def init_argparse() -> argparse.ArgumentParser:
